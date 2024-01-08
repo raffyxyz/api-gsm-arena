2bbd98a72f7ed799371cd65b31a01797c4e7f485
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-01-08 14:35:53 +0800

    new: gsm arena endpoint

--- src/controllers/gsm.ts
+++ src/controllers/gsm.ts
@@ -1,13 +1,26 @@
 import express from "express";
 
-import { scrapeSearch } from "../parser/gsm-parser";
+import { scrapeSearch, scrapeInfo } from "../parser/gsm-parser";
+import { formatSearch } from "../helper";
 
 export const search = async (req: express.Request, res: express.Response) => {
+  const query = req.query.q;
   try {
-    const result = await scrapeSearch();
+    const result = await scrapeSearch(formatSearch(query as string));
     return res.status(200).json(result);
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
+  }
+};
+
+export const info = async (req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+  try {
+    const result = await scrapeInfo(id as string);
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
   }
 };
