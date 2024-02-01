d627a6b5eba2760d514e681ed3e3ad35662f8f51
Raffy <raffy.amoguis1@gmail.com>
2024-02-01 17:49:35 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,15 @@ server.listen(8080, () => {
 });
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.status(200).send("Welcome to gsm arena api 🎉");
+  res
+    .status(200)
+    .send(
+      `Welcome to gsm arena api 🎉 ${
+        process.env.NODE_ENV === "DEMO"
+          ? "This is a demo of the api for testing and development."
+          : ""
+      }`
+    );
 });
 
 app.use("/", router());
